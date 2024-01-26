5e0e374c60db10fcc78e17a270cf0897c8c17597
Wilber Jiménez Hernández <49035216+wilberj88@users.noreply.github.com>
2024-01-26 13:48:23 +0100

    Update Home.py

--- Home.py
+++ Home.py
@@ -18,7 +18,7 @@ import streamlit.components.v1 as com
 import sqlite3 as sq3
 import pandas.io.sql as pds
 
-path = st.connection('env:DB_URL', 'sql')
+path = st.connection('instacart_db', 'sql')
 
 con = sq3.Connection(path)
 
