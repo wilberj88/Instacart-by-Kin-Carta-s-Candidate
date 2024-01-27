ce6cce33d45fb7374c2f1c4314ca0d6abaf3cf80
Wilber Jiménez Hernández <49035216+wilberj88@users.noreply.github.com>
2024-01-27 23:54:39 +0100

    Create offers.py

---
+++ pages/offers.py
@@ -0,0 +1,26 @@
+import streamlit as st
+from streamlit_card import card
+from streamlit_extras.grid import grid
+from streamlit_extras.colored_header import colored_header
+import time
+from streamlit_echarts import st_echarts
+import pytrends
+from pytrends.request import TrendReq
+import requests
+import pandas as pd
+
+
+# SETTING PAGE CONFIG TO WIDE MODE AND ADDING A TITLE AND FAVICON
+st.set_page_config(layout="wide", page_title="Instacart Dasboard", page_icon="🛒")
+
+st.title('Instacart Dasboard 🛒')
+current_time = time.ctime()
+st.write("In real time monitoring at: ", current_time)
+
+st.header('Offers Strategy 🏷️')
+
+colored_header(
+    label="Historic Data",
+    description="By hot & cold clients",
+    color_name="violet-70",
+)
