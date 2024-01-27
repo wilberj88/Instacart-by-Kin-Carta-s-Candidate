ce2e054b8daaa9bf4203cbdf44f74c3f2131b3a1
Wilber Jiménez Hernández <49035216+wilberj88@users.noreply.github.com>
2024-01-28 00:42:17 +0100

    Update products.py

--- pages/products.py
+++ pages/products.py
@@ -8,7 +8,8 @@ import pytrends
 from pytrends.request import TrendReq
 import requests
 import pandas as pd
-
+import matplotlib.pyplot as plt
+import seaborn as sns
 
 # SETTING PAGE CONFIG TO WIDE MODE AND ADDING A TITLE AND FAVICON
 st.set_page_config(layout="wide", page_title="Instacart Dasboard", page_icon="🛒")
