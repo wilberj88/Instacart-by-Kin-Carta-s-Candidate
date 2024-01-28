4978a721f0c01537243ec31774e074266c2639a9
Wilber Jiménez Hernández <49035216+wilberj88@users.noreply.github.com>
2024-01-28 03:28:13 +0100

    Update dashboard.py

--- pages/dashboard.py
+++ pages/dashboard.py
@@ -82,8 +82,8 @@ with col5:
                 )
          
         df_orders =  pd.read_csv("data/Orders.csv", index_col=0)
-        st.bar_chart(df_orders['order_hour_of_day'])
-       
+        st.bar_chart(df_orders, x="order_hour_of_day", y="frecuency")
+
                    
         
 with col6:
