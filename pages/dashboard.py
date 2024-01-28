b534ffffa8f3618aa3ed24ea88510512cf7aef57
Wilber Jiménez Hernández <49035216+wilberj88@users.noreply.github.com>
2024-01-28 02:01:49 +0100

    Create dashboard.py

---
+++ pages/dashboard.py
@@ -0,0 +1,30 @@
+import streamlit as st
+import requests
+from streamlit_echarts import st_echarts
+import pandas as pd
+from lightweight_charts.widgets import StreamlitChart
+import random
+import numpy as np
+import pandas as pd
+import time as ts
+from datetime import time
+import plotly.graph_objects as go
+import pydeck as pdk
+import folium
+from streamlit_folium import st_folium
+from streamlit_extras.grid import grid
+from streamlit_extras.colored_header import colored_header
+import streamlit.components.v1 as com
+import sqlite3 as sq3
+import pandas.io.sql as pds
+from PIL import Image
+
+st.set_page_config(
+    layout = 'wide',
+    page_title = '🛒 Instacart Dashboard Demo'
+)
+
+with st.sidebar:
+    image = Image.open('KinCartalogo.png')
+    st.image(image, caption='Demo sourced by 🛒 Instacart DB and APIs of 🌧️ Open Weather and 🔎 Google Trends in Real Time 🏭 by candidate Wilber Jiménez Hernández')
+  
