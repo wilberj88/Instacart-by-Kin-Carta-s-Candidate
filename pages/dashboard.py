d513f151064a7ec847b9fdff59a37e19d4e98197
Wilber Jiménez Hernández <49035216+wilberj88@users.noreply.github.com>
2024-01-28 02:02:20 +0100

    Update dashboard.py

--- pages/dashboard.py
+++ pages/dashboard.py
@@ -28,3 +28,23 @@ with st.sidebar:
     image = Image.open('KinCartalogo.png')
     st.image(image, caption='Demo sourced by 🛒 Instacart DB and APIs of 🌧️ Open Weather and 🔎 Google Trends in Real Time 🏭 by candidate Wilber Jiménez Hernández')
   
+# SETTING PAGE CONFIG TO WIDE MODE AND ADDING A TITLE AND FAVICON
+st.set_page_config(layout="wide", page_title="Instacart Dasboard", page_icon="🛒")
+
+st.title('Instacart Dasboard 🛒')
+current_time = time.ctime()
+st.write("In real time monitoring at: ", current_time)
+
+st.header('Product Strategy 📦')
+
+colored_header(
+    label="Historic Data - Purchases",
+    description="By days and hot & cold trends",
+    color_name="violet-70",
+)
+
+col1, col2, col3, col4 = st.columns(4)
+col1.metric("Best Hour", "10:00", "20%")
+col2.metric("Best Day", "Sunday", "33%")
+col3.metric("Best Client", "ID 72.726", "25%")
+col4.metric("Best Product", "ID 24.852", "17%")
